f0b1966cd9dd704a95b02f60619c4bf5f4360f03
fahri <fahri-can@gmx.at>
2022-08-16 21:26:32 +0200

    Start migration.

---
+++ src/services/TaskApi.ts
@@ -0,0 +1,29 @@
+import type { TaskRequest } from '~/types/TaskRequest'
+import Api from '~/services/Api'
+
+export default {
+  getTask(id: number) {
+    return Api().get(`/api/task/${id}`)
+  },
+  getOpenTasks() {
+    return Api().get('/api/open-tasks')
+  },
+  getClosedTasks() {
+    return Api().get('/api/closed-tasks')
+  },
+  getAllTasks() {
+    return Api().get('/api/all-tasks')
+  },
+  createTask(data: TaskRequest) {
+    return Api().post('/api/create', data)
+  },
+  deleteTask(id: number) {
+    return Api().delete(`/api/delete/${id}`)
+  },
+  updateTaskWithObject(data: TaskRequest) {
+    return Api().put('/api/update', data)
+  },
+  updateTaskWithUri(id: number, data: TaskRequest) {
+    return Api().put(`/api/update/${id}`, data)
+  },
+}
